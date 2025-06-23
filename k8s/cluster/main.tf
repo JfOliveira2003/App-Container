53ba745b256273fe710e93ff7e32d280fdf12373
fabricio.jose <fabricio.jose@a4pm.com.br>
2025-06-23 11:20:12 -0300

    .

--- k8s/cluster/main.tf
+++ k8s/cluster/main.tf
@@ -1,19 +1,19 @@
 terraform {
+  required_version = "~> 1.0"
   required_providers {
     digitalocean = {
-      source = "digitalocean/digitalocean"
-      version = "2.55.0"
+      source  = "digitalocean/digitalocean"
+      version = "~>2.0"
     }
   }
 }
 
 provider "digitalocean" {
-  token = "dop_v1_60531b30c287fcfec43d7ea2e0deb9877951235bf19d2e3f82eaca57add40794"
 }
 
-resource "digitalocean_kubernetes_cluster" "foo" {
-  name   = "foo"
-  region = "nyc1"
+resource "digitalocean_kubernetes_cluster" "k8s-cluster" {
+  name    = "k8s-cluster"
+  region  = "nyc1"
   version = "latest"
 
   node_pool {
@@ -21,14 +21,9 @@ resource "digitalocean_kubernetes_cluster" "foo" {
     size       = "s-2vcpu-2gb"
     node_count = 3
 
-    taint {
-      key    = "workloadKind"
-      value  = "database"
-      effect = "NoSchedule"
-    }
   }
 }
 output "kubeconfig" {
-  value     = digitalocean_kubernetes_cluster.foo.kube_config[0].raw_config
+  value     = digitalocean_kubernetes_cluster.k8s-cluster.kube_config[0].raw_config
   sensitive = true
 }
